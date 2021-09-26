083592b5f97b5058981a16293f5f3e7d6591405c
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-26 10:48:48 +0530

    ⚡

---
+++ pages/[pid].js
@@ -0,0 +1,14 @@
+import { Fragment, useEffect } from 'react';
+
+const ProductDetailPage = () => {
+    useEffect();
+
+  return (
+    <Fragment>
+      <h1>title</h1>
+      <p>Description</p>
+    </Fragment>
+  );
+};
+
+export default ProductDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
-const index = () => {
+import path from 'path';
+import fs from 'fs/promises';
+
+const index = (props) => {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Page 1</li>
-      <li>Page 2</li>
-      <li>Page 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 };
 
-export async function getStaticProps() {
-  return { props };
+export async function getStaticProps(context) {
+  console.log('(re)-generating....');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend-data.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
 
 export default index;
